12a2ebe62f6a49c67f6db4a516665327cb7dfe85
dq78 <devq78@gmail.com>
2022-10-14 14:58:57 +0200

    btn cyberpunk style

--- js/script.js
+++ js/script.js
@@ -4,3 +4,44 @@ if ('IntersectionObserver' in window) {
 } else {
     console.log("Your browser does not support IntersectionObserver");
 }
+
+
+
+
+
+
+/*
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    displayContents(contents);
+  };
+  reader.readAsText(file);
+}
+
+function displayContents(contents) {
+  var element = document.getElementById('file-content');
+  element.textContent = contents;
+}
+
+document.getElementById('file-input')
+  .addEventListener('change', readSingleFile, false);
+<input type="file" id="file-input" />
+<h3>Contents of the file:</h3>
+<pre id="file-content"></pre>
+
+*/
+
+/*
+var fileContent = "My epic novel that I don't want to lose.";
+var bb = new Blob([fileContent ], { type: 'text/plain' });
+var a = document.createElement('a');
+a.download = 'download.txt';
+a.href = window.URL.createObjectURL(bb);
+a.click();
+*/
